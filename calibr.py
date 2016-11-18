9234d52244f7420aa75d66f6d3d8f9251c35cf74
comRamona <com.ramona@yahoo.com>
2016-11-18 18:17:53 +0000

    added gyro angle

---
+++ calibr.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# Core imports
+import time
+import ev3dev.ev3 as ev3
+
+# Local Imports
+import tutorial as tutorial
+import utilities
+import openLoopControl as olc
+
+
+motL = ev3.LargeMotor('outD')
+btn=ev3.Button()
+g=ev3.GyroSensor()
+g.mode='GYRO-ANG'
+oldVal=g.value()
+print("OLD VALUE: ",oldVal)
+while(oldVal-g.value()<89):
+    motL.run_direct(duty_cycle_sp=20)
+    print(g.value())
+    if btn.any():
+        break;
+
+
+motL.stop()
--- compile.sh
+++ compile.sh
@@ -1,5 +1,9 @@
-cd IVRWalnut
-chmod +x lineFollower.py
+
+#chmod +x lineFollower.py
+#chmod +x pid.py
+chmod +x pid2.py
+chmod +x turn.py
+chmod +x calibr.py
 cd ..
 scp -r IVRWalnut robot@ev3dev:/home/robot/
 

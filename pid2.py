990db0b9bd1821edb6c7f40c04ea3a45993fb11a
comRamona <com.ramona@yahoo.com>
2016-11-18 13:29:19 +0000

    workk

--- pid2.py
+++ pid2.py
@@ -43,7 +43,7 @@ def steering2(course, power):
 			power_right = power
 			power_left = power + ((power * course) / 100)
 	return (int(power_left), int(power_right))
-Tp = 50
+Tp = 60
 motR = ev3.LargeMotor('outA')
 motL = ev3.LargeMotor('outD')
 colorSensor = ev3.ColorSensor()
@@ -51,9 +51,9 @@ btn =ev3.Button()
 colorSensor.mode = 'COL-REFLECT'
 colorSensor.connected
 runForward()
-Kp = 1000.0                             # REMEMBER we are using Kp*100 so this is really 10 !
-Ki = 100.0                              # REMEMBER we are using Ki*100 so this is really 1 !
-Kd = 10000.0                            # REMEMBER we are using Kd*100 so this is really 100!
+Kp = float(2) # Proportional gain. Start value 1
+Kd =0.5           # Derivative gain. Start value 0
+Ki = float(0.5) # Integral gain. Start value 0                        # REMEMBER we are using Kd*100 so this is really 100!
 offset = 45                           # Initialize the variables
 integral = 0.0                          # the place where we will store our integral
 lastError = 0.0                         # the place where we will store the last error value
@@ -61,14 +61,14 @@ derivative = 0.0                        # the place where we will store the deri
 while not btn.any():
    LightValue = colorSensor.value()    # what is the current light reading?
    error = LightValue - offset        # calculate the error by subtracting the offset
-   integral = integral + error        # calculate the integral
+   integral = 0.5*integral + error        # calculate the integral
    derivative = error - lastError     # calculate the derivative
    Turn = Kp*error + Ki*integral + Kd*derivative  # the "P term" the "I term" and the "D term"
-   Turn = Turn/100                      # REMEMBER to undo the affect of the factor of 100 in Kp, Ki and Kd!
+   #Turn = Turn/100                      # REMEMBER to undo the affect of the factor of 100 in Kp, Ki and Kd!
   # powerA = Tp + Turn                 # the power level for the A motor
   # powerC = Tp - Turn                 # the power level for the C motor
    (l,r)=steering2(Turn,Tp)
    motR.duty_cycle_sp=r
    motL.duty_cycle_sp=l
    lastError = error                  #save the current error so it can be the lastError next time around
-   time.sleep(0.1)
+   #time.sleep(0.1)

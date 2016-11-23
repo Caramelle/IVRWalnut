12de77ee36fb1cbfda3cf66da6d4b4725d1060b2
caramelle <andreea.cucu@gmail.com>
2016-11-23 02:28:05 +0000

    goes around object in a circle

--- lastworkingC.py
+++ lastworkingC.py
@@ -51,7 +51,7 @@ def turnLeft(g,x, dist):
 	#print("OLD VALUE: ",oldVal)
 	while(math.fabs(oldVal - g.value())< 90 + x):
 		#print uS.value()
-		#print math.fabs(oldVal-g.value())," ", oldVal, " ", g.value()
+		print math.fabs(oldVal-g.value())," ", oldVal, " ", g.value()
 		motR.run_direct(duty_cycle_sp= -50)
 		#print(g.value())
 		if btn.any():
@@ -67,7 +67,7 @@ def turnRight(g, x, dist):
 	oldVal=g.value()
 	#print("OLD VALUE: ",oldVal)
 	while(math.fabs(oldVal-g.value())<90 + x):
-		#print math.fabs(oldVal-g.value())
+		print math.fabs(oldVal-g.value())
 		motL.run_direct(duty_cycle_sp=50)
 		if uS.value() <= dist:
 			motR.stop()
@@ -90,6 +90,193 @@ def checkEndLine(error, lastError):
 		#ev3.Sound.speak('I\'m done b').wait()
 		return 1
 
+
+
+def followLine():
+	Kp = float(0.25) # Proportional gain. Start value 1
+	Kd = 0.15           # Derivative gain. Start value 0
+	Ki = float(0.02) # Integral gain. Start value 0                        # REMEMBER we are using Kd*100 so this is really 
+	offset = 45                          # Initialize the variables
+	integral = 0.0                          # the place where we will store our integral
+	lastError = 0.0                         # the place where we will store the last error value
+	derivative = 0.0                        # the place where we will store the derivative
+	constantCount = 0
+	state="Time,Turn\n"
+	while not btn.any():
+		LightValue = colorSensor.value()    # what is the current light reading?
+		error = LightValue - offset        # calculate the error by subtracting the offset
+		
+		if uS.value() < 70:
+			motR.stop()
+			motL.stop()
+			break
+		integral = 0.5*integral + error        # calculate the integral
+		derivative = error - lastError     # calculate the derivative
+		
+		Turn = Kp*error + Ki*integral + Kd*derivative  # the "P term" the "I term" and the "D term"
+		powerA=Tp+Turn
+		powerC=Tp-Turn
+		if powerA > 100:
+			powerA = 100
+		if powerC > 100:
+			powerC = 100
+		if powerA < -100:
+			powerA = -100
+		if powerC < -100:
+			powerC = -100
+	 	motR.duty_cycle_sp=powerA
+		motL.duty_cycle_sp=powerC
+		state=state+str(time.time())+","+str(Turn)+"\n"
+		lastError = error                  #save the current error so it can be the lastError next time around
+		#time.sleep(0.1)
+
+def findObj(dist):
+	runForward()
+	while True:
+		motR.duty_cycle_sp = 30
+		motL.duty_cycle_sp = 30
+		print uS.value()
+		if uS.value() <= dist:
+			motL.stop()
+			motR.stop()
+			break
+	motR.stop()
+	motL.stop()
+
+def runForward():
+	motR.duty_cycle_sp = 30
+	motL.duty_cycle_sp = 30
+	motR.run_direct()
+	motL.run_direct()
+
+def moveAway():
+	Tp = 30
+	#ev3.Sound.speak('moving away from object').wait()
+	Kp = float(0.22) # Proportional gain. Start value 1
+	Kd = 0.15           # Derivative gain. Start value 0
+	Ki = float(0.02) # Integral gain. Start value 0                        # REMEMBER we are using Kd*100 so this is really 
+	offset = 70                            # Initialize the variables
+	integral = 0.0                          # the place where we will store our integral
+	lastError = 0.0                         # the place where we will store the last error value
+	derivative = 0.0                        # the place where we will store the derivative
+	constantCount = 0
+	state="Time,Turn\n"
+	while not btn.any():
+		DistanceValue = uS.value()    # what is the current light reading?
+		error = DistanceValue - offset        # calculate the error by subtracting the offset
+		print(error)
+		# if error == (100-offset):
+		# 	constantCount = constantCount + 1
+		# else:
+		# 	constantCount = 0
+		# if constantCount == 30:
+		# 	#ev3.Sound.speak('I\'m done b').wait()
+		# 	motR.stop()
+		# 	motL.stop()
+		# 	break
+		if DistanceValue >= 1000:
+			break
+		integral = 0.5*integral + error        # calculate the integral
+		derivative = error - lastError     # calculate the derivative
+		Turn = Kp*error + Ki*integral + Kd*derivative  # the "P term" the "I term" and the "D term"
+	    #powerA=Tp+Turn
+		#Turn = Turn/100                      # REMEMBER to undo the affect of the factor of 100 in Kp, Ki and Kd!
+		# powerA = Tp + Turn                 # the power level for the A motor
+		# powerC = Tp - Turn                 # the power level for the C motor
+	    #powerA=Tp+Turn
+		powerA=Tp+Turn
+		powerC=Tp-Turn
+		if powerA > 100:
+			powerA = 100
+		if powerC > 100:
+			powerC = 100
+		if powerA < -100:
+			powerA = -100
+		if powerC < -100:
+			powerC = -100
+		if uS.value() > 500:
+			motR.stop()
+			motL.stop()
+			break
+		motR.duty_cycle_sp=powerA
+		motL.duty_cycle_sp=powerC
+		state=state+str(time.time())+","+str(Turn)+"\n"
+		lastError = error                  #save the current error so it can be the lastError next time around
+	#time.sleep(0.1)
+	# while uS.value()<200:
+	# 	motR.run_direct()
+	# 	motL.run_direct()
+	motL.stop()
+	motR.stop()	
+
+def moveBody():
+	oldTime=time.time()
+	while(math.fabs(time.time()-oldTime)<1):
+		motL.run_direct(duty_cycle_sp=30)
+		motR.run_direct(duty_cycle_sp=25)
+	motL.stop()
+	motR.stop()
+
+
+
+def turnEyes():
+	oldPos=eyes.position
+	while(math.fabs(oldPos-eyes.position)<91):
+		eyes.run_direct()
+	eyes.stop()
+
+
+
+
+#motors and sensors
+motR = ev3.LargeMotor('outA')
+motL = ev3.LargeMotor('outD')
+uS = ev3.UltrasonicSensor()
+uS.mode = 'US-DIST-CM'
+colorSensor = ev3.ColorSensor()
+eyes = ev3.MediumMotor('outC')
+eyes.duty_cycle_sp=60
+g = ev3.GyroSensor()
+btn =ev3.Button()
+colorSensor.mode = 'COL-REFLECT'
+
+# calibration = open('calibration.txt', 'r')
+# #global values for calibration
+blackMin = 0;
+blackMax = 15;
+whiteMin = 95;
+whiteMax = 100;
+
+Tp = 30
+ev3.Sound.speak('Place me on the line').wait()
+time.sleep(1)
+runForward()
+followLine()
+ev3.Sound.speak('Found object. Turning left').wait()
+turnLeft(g, -2, 100)
+turnEyes()
+runForward()
+moveAway()
+# ev3.Sound.speak('moving my body').wait()
+# moveBody()
+# ev3.Sound.speak('turning right').wait()
+#turnRight(g,10, 200)
+# runForward()
+# # #ev3.Sound.speak('lookin for object').wait()
+# # #findObj(200)
+# # #ev3.Sound.speak('found object').wait()
+# # moveBody()
+# # ev3.Sound.speak('moving away from object').wait()
+# moveAway()
+# ev3.Sound.speak('moving my body').wait()
+# moveBody()
+# ev3.Sound.speak('turrning right').wait()
+# turnRight(g, 0, 100)
+# runForward()
+# findNextLine(colorSensor)
+# followLine()
+
+#UNUSED CODE:
 # def followLine():
 # 	#ev3.Sound.speak('Following line').wait()
 # 	Kp = float(2) # Proportional gain. Start value 1
@@ -133,182 +320,6 @@ def checkEndLine(error, lastError):
 # 	time.sleep(0.1)
 # 	#return 0
 
-def followLine():
-	Kp = float(0.25) # Proportional gain. Start value 1
-	Kd = 0.15           # Derivative gain. Start value 0
-	Ki = float(0.02) # Integral gain. Start value 0                        # REMEMBER we are using Kd*100 so this is really 
-	offset = 45                           # Initialize the variables
-	integral = 0.0                          # the place where we will store our integral
-	lastError = 0.0                         # the place where we will store the last error value
-	derivative = 0.0                        # the place where we will store the derivative
-	constantCount = 0
-	state="Time,Turn\n"
-	while not btn.any():
-		LightValue = colorSensor.value()    # what is the current light reading?
-		error = LightValue - offset        # calculate the error by subtracting the offset
-		# print(error)
-		# if error == (100-offset):
-		# 	constantCount = constantCount + 1
-		# else:
-		# 	constantCount = 0
-		# if constantCount == 30:
-		# 	#ev3.Sound.speak('I\'m done b').wait()
-		# 	motR.stop()
-		# 	motL.stop()
-		# 	break
-		if uS.value() < 40:
-			motR.stop()
-			motL.stop()
-			break
-		integral = 0.5*integral + error        # calculate the integral
-		derivative = error - lastError     # calculate the derivative
-		
-		Turn = Kp*error + Ki*integral + Kd*derivative  # the "P term" the "I term" and the "D term"
-	    #powerA=Tp+Turn
-		#Turn = Turn/100                      # REMEMBER to undo the affect of the factor of 100 in Kp, Ki and Kd!
-		# powerA = Tp + Turn                 # the power level for the A motor
-		# powerC = Tp - Turn                 # the power level for the C motor
-	    #powerA=Tp+Turn
-		powerA=Tp+Turn
-		powerC=Tp-Turn
-		if powerA > 100:
-			powerA = 100
-		if powerC > 100:
-			powerC = 100
-		if powerA < -100:
-			powerA = -100
-		if powerC < -100:
-			powerC = -100
-	 	motR.duty_cycle_sp=powerA
-		motL.duty_cycle_sp=powerC
-		state=state+str(time.time())+","+str(Turn)+"\n"
-		lastError = error                  #save the current error so it can be the lastError next time around
-		#time.sleep(0.1)
-
-def findObj(dist):
-	runForward()
-	while True:
-		motR.duty_cycle_sp = 30
-		motL.duty_cycle_sp = 30
-		print uS.value()
-		if uS.value() <= dist:
-			motL.stop()
-			motR.stop()
-			break
-	motR.stop()
-	motL.stop()
-
-def checkLine():
-	global constantWhite
-	if checkBlack(colorSensor):
-		constantWhite = constantWhite + 1
-	else:
-		constantWhite = 0
-		return 0
-	if constantWhite == 20:
-		#ev3.Sound.speak('I\'m done b').wait()
-		return 1
-
-def runForward():
-	motR.duty_cycle_sp = 15
-	motL.duty_cycle_sp = 15
-	motR.run_direct()
-	motL.run_direct()
-
-def moveAway():
-	Tp = 30
-	Kt = float(0.0145)
-	Kp = float(0.22) # Proportional gain. Start value 1 ##0.22 0.15 0.02
-	Kd = float(0.15)        # Derivative gain. Start value 0
-	Ki = float(0.1) # Integral gain. Start value 0                        # REMEMBER we are using Kd*100 so this is really 
-	offset = 40                            # Initialize the variables
-	integral = 0.0                          # the place where we will store our integral
-	lastError = 0.0                         # the place where we will store the last error value
-	derivative = 0.0                        # the place where we will store the derivative
-	tangent = 0.0
-	constantCount = 0
-	lastTurn = 0.0
-	state="Time,Turn\n"
-	while not btn.any():
-		DistanceValue = uS.value()    # what is the current light reading?
-		error = DistanceValue - offset        # calculate the error by subtracting the offset
-		#print error
-		# if error == (100-offset):
-		# 	constantCount = constantCount + 1
-		# else:
-		# 	constantCount = 0
-		# if constantCount == 30:
-		# 	#ev3.Sound.speak('I\'m done b').wait()
-		# 	motR.stop()
-		# 	motL.stop()
-		# 	break
-		integral = 0.5*integral + error        # calculate the integral
-		derivative = error - lastError     # calculate the derivative
-		tangent = error * Kt
-		Turn = (Kp*error + Ki*integral + Kd*derivative)*Kt  # the "P term" the "I term" and the "D term"
-	    #powerA=Tp+Turn
-		#Turn = Turn/100                      # REMEMBER to undo the affect of the factor of 100 in Kp, Ki and Kd!
-		# powerA = Tp + Turn                 # the power level for the A motor
-		# powerC = Tp - Turn                 # the power level for the C motor
-	    #powerA=Tp+Turn
-		if checkLine():
-			motL.stop()
-			motR.stop()	
-			break
-		print Turn
-		powerA=Tp+Turn
-		powerC=Tp-Turn
-		if powerA > 100:
-			powerA = 100
-		if powerC > 100:
-			powerC = 100
-		if powerA < -100:
-			powerA = -100
-		if powerC < -100:
-			powerC = -100
-		#handle corners
-		# if error == 2400:
-		# 	aux = powerA
-		#  	powerA = powerC 
-		#  	powerC = aux 
-		# print powerA, powerC
-		# if error > 1000:
-		# 	constantCount = constantCount + 1;
-		# else:
-		# 	constantCount = 0
-		# if constantCount == 500:
-		# 	motR.stop()
-		# 	motL.stop()
-		# 	break
-		motR.duty_cycle_sp=powerA
-		motL.duty_cycle_sp=powerC
-		state=state+str(time.time())+","+str(Turn)+"\n"
-		lastError = error                  #save the current error so it can be the lastError next time around
-	#time.sleep(0.1)
-	# while uS.value()<200:
-	# 	motR.run_direct()
-	# 	motL.run_direct()
-	ev3.Sound.speak('This is the line').wait()
-	motL.stop()
-	motR.stop()	
-
-def moveBody():
-	oldTime=time.time()
-	while(math.fabs(time.time()-oldTime)<1):
-		motL.run_direct(duty_cycle_sp=30)
-		motR.run_direct(duty_cycle_sp=25)
-	motL.stop()
-	motR.stop()
-
-
-
-def turnEyes():
-	oldPos=eyes.position
-	while(math.fabs(oldPos-eyes.position)<85):
-		eyes.run_direct()
-	eyes.stop()
-
-
 # def steering2(course, power):
 # 	"""
 # 	Computes how fast each motor in a pair should turn to achieve the
@@ -344,55 +355,3 @@ def turnEyes():
 # 			power_right = power
 # 			power_left = power + ((power * course) / 100)
 # 	return (int(power_left), int(power_right))
-
-#motors and sensors
-motR = ev3.LargeMotor('outA')
-motL = ev3.LargeMotor('outD')
-uS = ev3.UltrasonicSensor()
-uS.mode = 'US-DIST-CM'
-colorSensor = ev3.ColorSensor()
-eyes = ev3.MediumMotor('outC')
-eyes.duty_cycle_sp=60
-g = ev3.GyroSensor()
-btn =ev3.Button()
-colorSensor.mode = 'COL-REFLECT'
-
-# calibration = open('calibration.txt', 'r')
-# #global values for calibration
-blackMin = 0;
-blackMax = 15;
-whiteMin = 95;
-whiteMax = 100;
-constantWhite = 0
-
-Tp = 30
-ev3.Sound.speak('Place me on the line').wait()
-time.sleep(1)
-runForward()
-followLine()
-ev3.Sound.speak('Found object. Turning left').wait()
-turnLeft(g, -2, 100)
-turnEyes()
-ev3.Sound.speak('moving away from object').wait()
-runForward()
-moveAway()
-motR.stop()
-motL.stop()
-# ev3.Sound.speak('moving my body').wait()
-# moveBody()
-# ev3.Sound.speak('turning right').wait()
-# turnRight(g,10, 200)
-# runForward()
-# # #ev3.Sound.speak('lookin for object').wait()
-# # #findObj(200)
-# # #ev3.Sound.speak('found object').wait()
-# # moveBody()
-# # ev3.Sound.speak('moving away from object').wait()
-# moveAway()
-# ev3.Sound.speak('moving my body').wait()
-# moveBody()
-# ev3.Sound.speak('turrning right').wait()
-# turnRight(g, 0, 100)
-# runForward()
-# findNextLine(colorSensor)
-# followLine()
